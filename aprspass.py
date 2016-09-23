5d2b208b709c37d3bcac3bfe09199a59990dd736
Kenneth Finnegan <kennethfinnegan2007@gmail.com>
2016-09-23 00:42:14 -0700

    Changed whitespace to tabs

--- aprspass.py
+++ aprspass.py
@@ -10,23 +10,23 @@ import sys
 
 def getPass(callsign):
 
-        basecall = callsign.upper().split('-')[0] + '\0'
-        result = 0x73e2
-        
-        c = 0
-        while (c+1 < len(basecall)):
-                result ^= ord(basecall[c]) << 8
-                result ^= ord(basecall[c+1])
-                c += 2
-        
-        result &= 0x7fff
-        return result
+	basecall = callsign.upper().split('-')[0] + '\0'
+	result = 0x73e2
+
+	c = 0
+	while (c+1 < len(basecall)):
+		result ^= ord(basecall[c]) << 8
+		result ^= ord(basecall[c+1])
+		c += 2
+
+	result &= 0x7fff
+	return result
 
 def main():
-        try:
-                print (getPass(sys.argv[1]))
-        except:
-                print ("Usage: aprspass.py <callsign>")
+	try:
+		print (getPass(sys.argv[1]))
+	except:
+		print ("Usage: aprspass.py <callsign>")
 
 if __name__ == "__main__":
-        main()
+	main()
